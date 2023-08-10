9669c7d8abadb2baa4019d379fbafe876bc7411a
Mohammad Ibrahim <112249309+awesomecoderdev@users.noreply.github.com>
2023-08-10 17:44:15 +0600

    Updated Zones Finder

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \App\Http\Middleware\AcceptJson::class,
+            // \App\Http\Middleware\AcceptJson::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
